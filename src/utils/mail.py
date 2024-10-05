0834ca49fc3a67c48f727b40dfe0948c45f70af2
physeex <Physeex@gmail.com>
2024-10-05 16:55:20 +1000

    Feat&refactor: 1) Add reset_password router; 2) Rewrite the Celery task so that it calls a class (it's method) defined dynamically via the passed argument

--- src/utils/mail.py
+++ src/utils/mail.py
@@ -6,11 +6,26 @@ from pydantic import EmailStr
 from auth.models import User
 from config import (
     SMTP_HOST, SMTP_PORT,
-    SMTP_USER, SMTP_PASSWORD, VERIFY_URL
+    SMTP_USER, SMTP_PASSWORD,
+    VERIFY_URL, PASSWORD_RESET_URL
     )
 
 
 class EmailInterface():
+
+    @staticmethod
+    def _smtp_helper(
+        email: EmailMessage,
+        SMTP_HOST: str = SMTP_HOST,
+        SMTP_PORT: str = SMTP_PORT,
+        SMTP_USER: str = SMTP_USER,
+        SMTP_PASSWORD: str = SMTP_PASSWORD
+    ):
+        """ Helper function to send EmailMessage via SMTP. """
+
+        with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as email_server:
+            email_server.login(SMTP_USER, SMTP_PASSWORD)
+            email_server.send_message(email)
 
     @classmethod
     def get_email_template(
@@ -19,6 +34,7 @@ class EmailInterface():
         to_email: EmailStr,
         content: str
     ) -> EmailMessage:
+        """ Returns an EmailMessage ready to be sent. """
         email = EmailMessage()
         email["Subject"] = subject
         email["From"] = SMTP_USER
@@ -35,10 +51,7 @@ class EmailInterface():
         content: str
     ) -> None:
         email = cls.get_email_template(subject, to_email, content)
-
-        with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as email_server:
-            email_server.login(SMTP_USER, SMTP_PASSWORD)
-            email_server.send_message(email)
+        cls._smtp_helper(email)
 
 
 class EmailVerification(EmailInterface):
@@ -50,18 +63,19 @@ class EmailVerification(EmailInterface):
     def send_email(cls, user: User, token: str) -> None:
         content = (
             "<div>"
-            "<h3>"
+            "<p>"
             f"Hello, {user.first_name}!"
             "<br>"
             "Please, click on the link below to verify your email "
-            "if you're using UI (site):"
-            "</h3><br>"
+            "if you're using UI (site): "
+            "<br><br>"
             f"{cls.url}?{token}"
-            "<h3>"
+            "<br><br>"
             "If you're using the API make a POST request to "
             f"{cls.url} in json format with data:"
-            "</h3><br>"
+            "<br><br>"
             f'"token": "{token}"'
+            "</p>"
             "</div>"
         )
 
@@ -71,6 +85,42 @@ class EmailVerification(EmailInterface):
             content=content
         )
 
-        with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as email_server:
-            email_server.login(SMTP_USER, SMTP_PASSWORD)
-            email_server.send_message(email)
+        cls._smtp_helper(email)
+
+
+class EmailResetPassword(EmailInterface):
+
+    url = PASSWORD_RESET_URL
+    subject = "Password reset"
+
+    @classmethod
+    def send_email(cls, user: User, token: str) -> None:
+        content = (
+            "<div>"
+            "<p>"
+            f"Hello, {user.first_name}!"
+            "<br>"
+            "You're receiving this email because you/someone else "
+            "requested a password reset for your user account at "
+            "BugTracker API."
+            "<br><br>"
+            "Please go to this page and enter a new password:"
+            "<br><br>"
+            f"{cls.url}?{token}"
+            "<br><br>"
+            "If you're using the API make a POST request to "
+            f"{cls.url} in json format with data:"
+            "<br><br>"
+            f'"token": "{token}",'
+            "<br>"
+            f'"password": "YOUR NEW PASSWORD"'
+            "</p>"
+            "</div>"
+        )
+
+        email = cls.get_email_template(
+            subject=cls.subject,
+            to_email=user.email,
+            content=content
+            )
+        cls._smtp_helper(email)
--- src/utils/tasks.py
+++ src/utils/tasks.py
@@ -1,9 +1,14 @@
-from celery import shared_task
+from importlib import import_module
 
-from .mail import EmailVerification
-from auth.models import User
+from celery import shared_task
 
 
 @shared_task
-def celery_send_email(user: User, token: str):
-    EmailVerification.send_email(user=user, token=token)
+def celery_send_email(func: str, *func_args, **func_kwargs):
+    try:
+        email_module = import_module(".mail", "utils")
+        email_class = getattr(email_module, func)
+    except AttributeError:
+        raise
+    else:
+        email_class.send_email(*func_args, **func_kwargs)
