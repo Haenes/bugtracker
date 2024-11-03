dc7b13d22b24f5c4626a8efe699b69a8502f791e
physeex <Physeex@gmail.com>
2024-11-03 15:41:20 +1000

    Feat: customize fastapi-users further Add uniqueness to the username field and give the 400 error instead 500 in case of violation

--- backend/src/auth/custom.py
+++ backend/src/auth/custom.py
@@ -1,11 +1,12 @@
+from enum import Enum
 from typing import Type
+
+from sqlalchemy.exc import IntegrityError
 
 from fastapi import (
     APIRouter, Body, Depends,
     HTTPException, Request, Response, status
     )
-
-# from pydantic import EmailStr
 
 from fastapi_users import FastAPIUsers, exceptions, models, schemas
 from fastapi_users.authentication import Authenticator
@@ -13,8 +14,32 @@ from fastapi_users.manager import BaseUserManager, UserManagerDependency
 from fastapi_users.router.common import ErrorCode, ErrorModel
 
 
+class CustomErrorCode(str, Enum):
+    USERNAME_ALREADY_EXISTS = "USERNAME_ALREADY_EXISTS"
+
+
 class CustomFastAPIUsers(FastAPIUsers[models.UP, models.ID]):
-    """Class for calling the rewritten get_users_router function."""
+    """
+    Class for calling some custom functions
+    instead of the original ones.
+    """
+
+    def get_register_router(
+        self,
+        user_schema: type[schemas.U],
+        user_create_schema: type[schemas.UC]
+    ) -> APIRouter:
+        """
+        Return a custom router with a register route.
+
+        :param user_schema: Pydantic schema of a public user.
+        :param user_create_schema: Pydantic schema for creating a user.
+        """
+        return custom_get_register_router(
+            self.get_user_manager,
+            user_schema,
+            user_create_schema
+        )
 
     def get_users_router(
         self,
@@ -22,6 +47,14 @@ class CustomFastAPIUsers(FastAPIUsers[models.UP, models.ID]):
         user_update_schema: Type[schemas.UU],
         requires_verification: bool = False,
     ) -> APIRouter:
+        """
+        Return a custom router with routes to manage users.
+
+        :param user_schema: Pydantic schema of a public user.
+        :param user_update_schema: Pydantic schema for updating a user.
+        :param requires_verification: Whether the endpoints
+        require the users to be verified or not. Defaults to False.
+        """
 
         return custom_get_users_router(
             self.get_user_manager,
@@ -33,11 +66,99 @@ class CustomFastAPIUsers(FastAPIUsers[models.UP, models.ID]):
 
     def get_verify_router(self, user_schema: Type[schemas.U]) -> APIRouter:
         """
-        Return a router with e-mail verification route.
+        Return a custom router with e-mail verification route.
 
         :param user_schema: Pydantic schema of a public user.
         """
         return custom_get_verify_router(self.get_user_manager, user_schema)
+
+
+def custom_get_register_router(
+    get_user_manager: UserManagerDependency[models.UP, models.ID],
+    user_schema: Type[schemas.U],
+    user_create_schema: Type[schemas.UC],
+) -> APIRouter:
+    """
+    Generate a router with the register route.
+
+    CUSTOM: Add username uniqness check
+    """
+    router = APIRouter()
+
+    @router.post(
+        "/register",
+        response_model=user_schema,
+        status_code=status.HTTP_201_CREATED,
+        name="register:register",
+        responses={
+            status.HTTP_400_BAD_REQUEST: {
+                "model": ErrorModel,
+                "content": {
+                    "application/json": {
+                        "examples": {
+                            ErrorCode.REGISTER_USER_ALREADY_EXISTS: {
+                                "summary": "A user with this email already exists.",
+                                "value": {
+                                    "detail": ErrorCode.REGISTER_USER_ALREADY_EXISTS
+                                },
+                            },
+                            CustomErrorCode.USERNAME_ALREADY_EXISTS: {
+                                "summary": "A user with this username already exists.",
+                                "value": {
+                                    "detail": CustomErrorCode.USERNAME_ALREADY_EXISTS
+                                },
+                            },
+                            ErrorCode.REGISTER_INVALID_PASSWORD: {
+                                "summary": "Password validation failed.",
+                                "value": {
+                                    "detail": {
+                                        "code": ErrorCode.REGISTER_INVALID_PASSWORD,
+                                        "reason": "Password should be"
+                                        "at least 3 characters",
+                                    }
+                                },
+                            },
+                        }
+                    }
+                },
+            },
+        },
+    )
+    async def register(
+        request: Request,
+        user_create: user_create_schema,  # type: ignore
+        user_manager: BaseUserManager[
+            models.UP,
+            models.ID
+        ] = Depends(get_user_manager),
+    ):
+        try:
+            created_user = await user_manager.create(
+                user_create, safe=True, request=request
+            )
+        except exceptions.UserAlreadyExists:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=ErrorCode.REGISTER_USER_ALREADY_EXISTS,
+            )
+        except exceptions.InvalidPasswordException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "code": ErrorCode.REGISTER_INVALID_PASSWORD,
+                    "reason": e.reason,
+                },
+            )
+        except IntegrityError as e:
+            print("DETAIL OF EXCEPTION", e.detail)
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=CustomErrorCode.USERNAME_ALREADY_EXISTS,
+            )
+
+        return schemas.model_validate(user_schema, created_user)
+
+    return router
 
 
 def custom_get_users_router(
@@ -47,7 +168,9 @@ def custom_get_users_router(
     authenticator: Authenticator,
     requires_verification: bool = False,
 ) -> APIRouter:
-    """ Generate a router with the authentication routes.
+    """
+    Generate a router with the authentication routes.
+
     CUSTOM: Remove all admin routes.
     """
     router = APIRouter()
@@ -58,8 +181,7 @@ def custom_get_users_router(
 
     async def get_user_or_404(
         id: str,
-        user_manager:
-            BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
     ) -> models.UP:
         try:
             parsed_id = user_manager.parse_id(id)
@@ -77,9 +199,7 @@ def custom_get_users_router(
             },
         },
     )
-    async def me(
-        user: models.UP = Depends(get_current_active_user),
-    ):
+    async def me(user: models.UP = Depends(get_current_active_user),):
         return schemas.model_validate(user_schema, user)
 
     @router.patch(
@@ -97,11 +217,9 @@ def custom_get_users_router(
                     "application/json": {
                         "examples": {
                             ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS: {
-                                "summary": "A user with this "
-                                           "email already exists.",
+                                "summary": "A user with this email already exists.",
                                 "value": {
-                                    "detail":
-                                    ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS
+                                    "detail": ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS
                                 },
                             },
                             ErrorCode.UPDATE_USER_INVALID_PASSWORD: {
@@ -111,7 +229,7 @@ def custom_get_users_router(
                                         "code":
                                         ErrorCode.UPDATE_USER_INVALID_PASSWORD,
                                         "reason": "Password should be"
-                                        "at least 3 characters",
+                                        "at least 8 characters",
                                     }
                                 },
                             },
@@ -125,8 +243,7 @@ def custom_get_users_router(
         request: Request,
         user_update: user_update_schema,  # type: ignore
         user: models.UP = Depends(get_current_active_user),
-        user_manager:
-            BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
     ):
         try:
             user = await user_manager.update(
@@ -162,8 +279,7 @@ def custom_get_users_router(
     async def delete_user(
         request: Request,
         user: models.UP = Depends(get_current_active_user),
-        user_manager:
-            BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
     ):
         await user_manager.delete(user, request=request)
         return None
@@ -175,7 +291,9 @@ def custom_get_verify_router(
     get_user_manager: UserManagerDependency[models.UP, models.ID],
     user_schema: Type[schemas.U],
 ):
-    """ Generate the verification router.
+    """
+    Generate the verification router.
+
     CUSTOM: Remove /request-verify-token route.
     """
     router = APIRouter()
@@ -195,13 +313,12 @@ def custom_get_verify_router(
                                 "not the e-mail currently set for the user.",
                                 "value": {
                                     "detail": ErrorCode.VERIFY_USER_BAD_TOKEN
-                                    },
+                                },
                             },
                             ErrorCode.VERIFY_USER_ALREADY_VERIFIED: {
                                 "summary": "The user is already verified.",
                                 "value": {
-                                    "detail":
-                                        ErrorCode.VERIFY_USER_ALREADY_VERIFIED
+                                    "detail": ErrorCode.VERIFY_USER_ALREADY_VERIFIED
                                 },
                             },
                         }
